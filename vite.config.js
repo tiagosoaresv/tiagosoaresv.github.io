bd4f93712d84c8144ab4888bfbae2a618b3a9f6e
tiagosoaresv <a97381@gmail.com>
2025-05-08 00:54:36 +0100

    changed repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tiagosoares.github.io/',
+  base: '/tiagosoaresv.github.io/',
   plugins: [react()],
 })
